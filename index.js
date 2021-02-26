f228c9aead97a3caa0e56ab561d0198de64e125a
Artem  F <artemfm@gmail.com>
2021-02-26 17:41:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price = Math.floor(Math.random() * 101); 
+  cart.push({itemName: item, itemPrice: price});
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."}
+    else if (cart.length === 1){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    }
+
+  
+
   // write your code here
 }
 
